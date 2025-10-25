6bf3cca47f299011ef2b0569de4245b4f02d5ce0
SuccessPear <lethanhcong3920@gmail.com>
2025-10-25 10:11:29 +0200

    Add Roraty Positional encoding

--- modeling_gemma.py
+++ modeling_gemma.py
@@ -1,3 +1,5 @@
+from modulefinder import Module
+
 import torch
 from torch import nn
 from typing import Optional, Tuple, List
@@ -144,6 +146,54 @@ def repear_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
         return hidden_states
     hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+
+class GemmaRotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+
+        self.dim = dim
+        self.max_position_embeddings = max_position_embeddings
+        self.device = device
+        self.base = base
+
+        # Calculate the theta according to the formula theta_i = base^(2i/dim) where i = 0, 1, 2,..., dim//2
+        inv_freq = 1.0/(self.base ** (torch.arange(0, self.dim, 2, dtype=torch.int64).float()/self.dim))
+        self.register_buffer('inv_freq', tensor=inv_freq, persistent=False)
+    @torch.no_grad()
+    def forward(self, x, position_ids, seq_len = None):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        self.inv_freq.to(x.device)
+        # Copy the inv_freq tensor for bqtch in the sequence
+        # inv_freq_expanded [bz, 1, seq_len]
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+        # position_ids_expanded: [bz, 1, seq_len]
+        position_ids_expanded = position_ids[:, None, :].float()
+        device_type = x.device.type
+        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):
+            # Multiply each theta by the position (which is the argument of the sin and cos function)
+            # freqs: [bz, head_dim // 2, 1] @ [bz, 1, seq_len] --> [bz, seq_len, head_dim//2]
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            # emb: [bz, seq_len, head_dim]
+            emb = torch.cat((freqs, freqs), dim=-1)
+            # cos, sin: [bz, seq_len, head_dim]
+            cos = emb.cos()
+            sin = emb.sin()
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+
+def rotate_half(x):
+    # build the [-x2, x1, -x4, x3,...] tensor for the sin part of positional encoding
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.stack([x1, x2], dim=-1)
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    cos = cos.unsqueeze(unsqueeze_dim) # add the head dimension
+    sin = sin.unsqueeze(unsqueeze_dim)
+    # apply the formula (34) of the Rotary Positional Encoding paper
+    q_embed = (q*cos) + (rotate_half(q) * sin)
+    k_embed = (k*cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
 
 class GemmaAttention(nn.Module):
     def __init__(self, config: GemmaConfig, layer_idx: Optional[int]):
